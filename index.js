01202f93b5dca7a46065762fe66c8b1e0bcf166c
quincinia <61718793+quincinia@users.noreply.github.com>
2021-12-31 18:50:55 -0800

    Added middleware

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ const config = require('./utils/config')
 const Blog = require('./models/blog')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 // Now pulls url from config file instead of defining it here
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 const server = http.createServer(app)
 
