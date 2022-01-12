c9eb7dc10f50e2cf6544447cb605363794444ee8
quincinia <eanj13@gmail.com>
2022-01-11 20:58:24 -0800

    Finished and attached users router, requests work

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,11 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (req, res) => {
+    const users = await User.find({})
+    res.json(users)
+})
+
 usersRouter.post('/', async (req, res) => {
     const body = req.body
 
