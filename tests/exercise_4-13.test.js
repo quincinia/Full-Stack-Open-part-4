18d5e31364608a15b7e3e46a24964b55d288f3c0
quincinia <eanj13@gmail.com>
2022-01-15 19:52:10 -0800

    Correctly set the "user" field for each initial blog

--- tests/exercise_4-13.test.js
+++ tests/exercise_4-13.test.js
@@ -5,14 +5,15 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-
-    const blogs = helper.initialBlogs.map(blog => new Blog(blog))
+    const user = await User.findById(process.env.TEST_USER)
+    const blogs = helper.initialBlogs.map(blog => new Blog({ ...blog, user: user._id }))
     const blogsBeingSaved = blogs.map(blog => blog.save())
     await Promise.all(blogsBeingSaved)
-})
+}, 500000)
 
 test('note gets deleted', async () => {
     const blogsAtStart = await helper.blogsInDb()
@@ -20,6 +21,7 @@ test('note gets deleted', async () => {
 
     await api
         .delete(`/api/blogs/${blogToDelete.id}`)
+        .set('Authorization', 'bearer ' + process.env.TEST_TOKEN)
         .expect(204)
 
     const blogsAtEnd = await helper.blogsInDb()
