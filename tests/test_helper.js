7c7fb512d1032fd97306ce0d7e64399b345aa5cc
quincinia <eanj13@gmail.com>
2022-01-15 16:25:57 -0800

    initialBlogs now support "user" field

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,5 +1,6 @@
-const Blog = require('../models/blog')
+const mongoose = require('mongoose')
 
+const Blog = require('../models/blog')
 const initialBlogs = [
     {
         _id: '5a422a851b54a676234d17f7',
@@ -7,7 +8,8 @@ const initialBlogs = [
         author: 'Michael Chan',
         url: 'https://reactpatterns.com/',
         likes: 7,
-        __v: 0
+        __v: 0,
+        user: mongoose.Types.ObjectId(process.env.TEST_USER)
     },
     {
         _id: '5a422aa71b54a676234d17f8',
@@ -15,7 +17,8 @@ const initialBlogs = [
         author: 'Edsger W. Dijkstra',
         url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
         likes: 5,
-        __v: 0
+        __v: 0,
+        user: mongoose.Types.ObjectId(process.env.TEST_USER)
     },
     {
         _id: '5a422b3a1b54a676234d17f9',
@@ -23,7 +26,8 @@ const initialBlogs = [
         author: 'Edsger W. Dijkstra',
         url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
         likes: 12,
-        __v: 0
+        __v: 0,
+        user: mongoose.Types.ObjectId(process.env.TEST_USER)
     },
     {
         _id: '5a422b891b54a676234d17fa',
@@ -31,7 +35,8 @@ const initialBlogs = [
         author: 'Robert C. Martin',
         url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
         likes: 10,
-        __v: 0
+        __v: 0,
+        user: mongoose.Types.ObjectId(process.env.TEST_USER)
     },
     {
         _id: '5a422ba71b54a676234d17fb',
@@ -39,7 +44,8 @@ const initialBlogs = [
         author: 'Robert C. Martin',
         url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
         likes: 0,
-        __v: 0
+        __v: 0,
+        user: mongoose.Types.ObjectId(process.env.TEST_USER)
     },
     {
         _id: '5a422bc61b54a676234d17fc',
@@ -47,7 +53,8 @@ const initialBlogs = [
         author: 'Robert C. Martin',
         url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
         likes: 2,
-        __v: 0
+        __v: 0,
+        user: mongoose.Types.ObjectId(process.env.TEST_USER)
     }
 ]
 
